9086d88505e911b06a4943ca6ffa5cd636b4e826
Andrew Skurlatov <a.skurlatov@gmail.com>
2022-11-09 20:51:16 +0100

    feat: add meta and og tags

--- src/pages/_document.tsx
+++ src/pages/_document.tsx
@@ -3,13 +3,28 @@ import React from 'react';
 
 // noinspection JSUnusedGlobalSymbols
 export default function Document() {
-  // noinspection HtmlRequiredTitleElement
   return (
     <Html lang='en'>
       <Head>
-        <meta name='theme-color' content='white' />
-        <link rel='icon' href='./team/favicon.png' type='image/png' />
-        <link rel='icon' href='/team/favicon.png' type='image/png' />
+          {/* eslint-disable-next-line @next/next/no-title-in-document-head */}
+          <title>Blockchain, IT, Web3 Development Company | Blockchain Software Developers| Udduq team</title>
+          <meta name='theme-color' content='white' />
+          <link rel='icon' href='./team/favicon.png' type='image/png' />
+          <link rel='icon' href='/team/favicon.png' type='image/png' />
+          <meta property="og:title" content="Blockchain, IT, Web3 Development Company | Blockchain Software Developers| Udduq team"/>
+          <meta property="og:description" content="Production-focused development: blockchain projects, NFT projects, smart contracts, blockchain ecosystem apps, cryptocurrency wallets, and decentralized apps."/>
+          <meta property="og:type" content="website"/>
+          <meta property="og:site_name" content="Uddùg"/>
+          <meta property="og:url" content="https://uddug.com"/>
+          <meta property="og:image" content="./team/cover.jpg"/>
+          <meta name="twitter:title" content="Blockchain, IT, Web3 Development Company | Blockchain Software Developers| Udduq team"/>
+          <meta name="twitter:description" content="Production-focused development: blockchain projects, NFT projects, smart contracts, blockchain ecosystem apps, cryptocurrency wallets, and decentralized apps."/>
+          <meta name="twitter:image" content="./team/cover.jpg"/>
+          <meta name="twitter:card" content="summary_large_image"/>
+          <meta name="twitter:site" content="@uddug"/>
+          <meta name="twitter:image:alt" content="Uddùg team - we are devoted to creating help humanity get to the future faster"/>
+          <meta name="description" content="Production-focused development: blockchain projects, NFT projects, smart contracts, blockchain ecosystem apps, cryptocurrency wallets, and decentralized apps."/>
+          <meta name="keywords" content="It development team, Nft development team, Blockchain development team, Web3 development team "/>
       </Head>
       <link rel='preconnect' href='https://fonts.googleapis.com' />
       <link
