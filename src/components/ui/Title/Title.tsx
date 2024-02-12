6d8dc47c01f029d0db84c4ebcb08e3ac8a334fe2
KirllBashorin <99118225+KirllBashorin@users.noreply.github.com>
2024-02-12 12:02:31 +0400

    Title.tsx styles import fix

--- src/components/ui/Title/Title.tsx
+++ src/components/ui/Title/Title.tsx
@@ -1,7 +1,7 @@
 import { FC } from 'react';
 import classNames from 'classnames';
 
-import styles from './title.module.css';
+import styles from './Title.module.css';
 
 interface ITitleProps {
   children: React.ReactNode;
