bcc374ed7775c8590b5858caeb955b358ac9b71d
KirllBashorin <99118225+KirllBashorin@users.noreply.github.com>
2024-02-24 04:25:14 +0400

    Added api route - sendgrid

---
+++ src/pages/api/sendgrid.ts
@@ -0,0 +1,27 @@
+import sendgrid from '@sendgrid/mail';
+
+const apikey = process.env.SENDGRID_API_KEY;
+sendgrid.setApiKey(apikey ? apikey : '');
+
+async function sendEmail(req: any, res: any) {
+  try {
+    // console.log('REQ.BODY', req.body);
+    await sendgrid.send({
+      to: 'fumhejkeee@gmail.com', // Your email where you'll receive emails
+      from: 'qqwwee@itnerds.ru', // your website email address here
+      subject: `Lead from site`,
+      html: `<div>
+        <p>Name - ${req.body.name}</p>
+        <p>Email - ${req.body.email}</p>
+        <p>Comment - ${req.body.text}</p>
+      </div>`,
+    });
+  } catch (error: any) {
+    // console.log(error);
+    return res.status(error.statusCode || 500).json({ error: error.message });
+  }
+
+  return res.status(200).json({ error: '' });
+}
+
+export default sendEmail;
